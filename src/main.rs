67dada9c7f071873da5fbebfbab50b00352a7b01
Paulo <jimgr@riseup.net>
2024-03-26 17:23:20 +0100

    Fixed some thngs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::prelude::*;
 
 use serde_json::Value;
 
+/// Перевод base64 в string
 pub fn decode_url_base64_str(url_string: String) -> Result<String, FromUtf8Error>{
     use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
     let bytes_url = engine::GeneralPurpose::new(
@@ -14,6 +15,7 @@ pub fn decode_url_base64_str(url_string: String) -> Result<String, FromUtf8Error
     String::from_utf8(bytes_url)
 }
 
+/// Перевод base64 в byte
 pub fn decode_url_base64_byte(url_string: String) -> Result<Vec<u8>, base64::DecodeError> {
     use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
     engine::GeneralPurpose::new(
@@ -21,6 +23,7 @@ pub fn decode_url_base64_byte(url_string: String) -> Result<Vec<u8>, base64::Dec
                  general_purpose::NO_PAD).decode(url_string)
 }
 
+/// Перевод byte в base64
 pub fn encode_url_base64_byte(data: Vec<u8>) -> String{ 
     use base64::{Engine as _, alphabet, engine::{self, general_purpose}};
 
@@ -28,6 +31,9 @@ pub fn encode_url_base64_byte(data: Vec<u8>) -> String{
         &alphabet::URL_SAFE,
         general_purpose::NO_PAD).encode(data)
 }
+
+
+/// Распаковка zlib
 pub fn decode_zlib(data_to_decode: &[u8]) -> io::Result<String>{
     use flate2::read::ZlibDecoder;
     use std::io::prelude::*;
@@ -38,6 +44,7 @@ pub fn decode_zlib(data_to_decode: &[u8]) -> io::Result<String>{
     Ok(s)
 }
 
+/// Запаковка zlib
 pub fn encode_zlib(data: String) -> Result<Vec<u8>, io::Error> {
     use std::io::prelude::*;
     use flate2::Compression;
@@ -48,32 +55,29 @@ pub fn encode_zlib(data: String) -> Result<Vec<u8>, io::Error> {
     e.finish()
 }
 
+/// Обрезка текста конфига
 pub fn cut_config(data1: String, cut_number: usize) -> String{
     data1[cut_number..data1.len()].to_string()
 }
 
+/// Обрезка текста конфига по байтам
 pub fn cut_config_byte(data1: &[u8], cut_number: usize) -> &[u8]{
   &data1[cut_number..data1.len()]
 }
 
+/// Поиск и исправление информации
 fn json_fix(data: String) -> String{
     let a: Value = serde_json::from_str(&data).unwrap();
     let last_conf = a["containers"][0]["cloak"]["last_config"].to_string().replace("\\\"", "\"");
     let last_conf1 = &last_conf[1..last_conf.len() -1];
-    //let last_config: Value = serde_json::from_str(&last_conf.replace("\\\"", "\"")).unwrap();
     let last_config: Value = serde_json::from_str(&last_conf1).unwrap();
-    let openvpn_prep = &a["containers"][0]["openvpn"]["last_config"].to_string().replace("\\\\n", "\n").replace("\\\"", "\"");
+    let remote_host = last_config["RemoteHost"].to_string().replace("\"", ""); //Новые версии ругаются на доменное имя
 
+    let openvpn_prep = &a["containers"][0]["openvpn"]["last_config"].to_string().replace("\\\\n", "\n").replace("\\\"", "\"");
     let openvpn = openvpn_prep;
-    //let openvpn: Value = serde_json::from_str(&openvpn_prep[1..openvpn_prep.len()-1]).unwrap();
-    //println!("{}", a["containers"][0]["openvpn"]);
-   // println!("RemoteHost: {}", last_config["RemoteHost"]);
     let route = openvpn.split("route ").last().unwrap().split(" 255.255.255.255").nth(0).unwrap();
-    let remote_host = last_config["RemoteHost"].to_string().replace("\"", "");
-    //println!("\n========================\nRoute: {}\n========================", route);
-    //println!("RemoteHost: {}\n========================", remote_host);
-    let new_data = data.replace(&remote_host,  route);
-    //println!("\n\n\n{}",  new_data);
+
+    let new_data = data.replace(&remote_host,  route); //  Его нужно заменить на адрес, который можем взять в этом же конфиге в Openvpn > LastConfig
     new_data
 }
 
@@ -81,36 +85,45 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() > 1{
         let file_name = args[1].clone();
-        match File::open(&file_name){
+        match File::open(&file_name){ //Скучные проверки
             Ok(mut file1) => {
                 let mut data1 = String::new();
-                match file1.read_to_string(&mut data1) {
-                    Ok(_) => {
-                        if data1.contains("vpn://"){
+                match file1.read_to_string(&mut data1) { //Скучные проверки
+                    Ok(_) => { 
+                        if data1.contains("vpn://"){ //Убираем vpn:// из начала файла (На самом деле не только из начала)
                             if data1.find("vpn://").unwrap() == 0_usize {
                                 data1 = data1.replace("vpn://", "")
                             }
                         }
-                        if data1.ends_with("\n") {
+                        if data1.ends_with("\n") { //И убираем перенос из конца файла. Я не знаю зачем он там нужен, но с ним не переводится из base64
                             data1 = data1.replace("\n","");
                         }
-                        //println!("{:?}",data1);
+                        
                         let data2 = decode_url_base64_byte(data1.to_string()).unwrap();
                         let debase64 = cut_config_byte(&data2, 4);
                         let decode64 = decode_zlib(debase64).unwrap();
-                  //      println!("========================================================================================\n\nData: \n{:?}\n\n========================================================================================\n\nDecode Data: \n\n{}", debase64, decode64);
+                  
                         let fixed_data = json_fix(decode64);
                         let mut encoded_data = encode_zlib(fixed_data).unwrap();
+
+                        // Вот тут началось шапито. Дело в том, что если ты 
+                        // хочешь преобразовать файлик zlib в qcompress, 
+                        // то тебе нужно добавить 4 байта которые
+                        // показывают длину зашифрованного участкка
+                        // Вот тут про это написано https://doc.qt.io/qt-6/qbytearray.html#qUncompress-1
                         encoded_data.reverse();
                         encoded_data.push(255);
                         encoded_data.push(0);
                         encoded_data.push(0);
                         encoded_data.push(0);
                         encoded_data.reverse();
-                    //    print!("{:?}", encoded_data);
+
+                        // Я может выведу распаковку / Запаковку qcompress отдельно, но, пока что, пусть так будет.
+                    
+                    
                         let encoded_data1 = encode_url_base64_byte(encoded_data);
-                        // println!("\n\n========================================================================================\n{}", encoded_data1);
-                        match File::create(format!("new_{}", file_name)) {
+                        
+                        match File::create(file_name.replace(".vpn", ".fixed.vpn")) { //Скучные проверки
                             Ok(mut file2) => {
                                 match file2.write_all(format!("vpn://{}\n", &encoded_data1).as_bytes()) {
                                     Ok(_) => (),
@@ -129,7 +142,4 @@ fn main() {
     else{
         println!("Error(")
     }
-    // Decode
-    
-
 }
